4a47f04ad07d51971697ea46986d8772a25f4434
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-06-26 16:43:11 +0200

    feat: Ajout de la vérification des credentials au login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,20 +1,35 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Login.css"
 import logo from "../Logo_Hello_Kitchen.png"
+import axios from "axios"
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:3000/api/login/', {username, password})
+        .then((res) => {
+            console.log("GOOD", res)
+        })
+        .catch((err) => {
+            console.log("ERROR", err)
+        })
+    }
+
     return (
         <div className="login-container">
             <img src={logo} alt="Logo" className="login-logo" />
             <div className="login-box">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-field">
                         <label htmlFor="username">Nom d'utilisateur</label>
-                        <input type="text" id="username" name="username" />
+                        <input type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)}/>
                     </div>
                     <div className="input-field">
                         <label htmlFor="password">Mot de passe</label>
-                        <input type="password" id="password" name="password" />
+                        <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <button type="submit">Se connecter</button>
                 </form>
