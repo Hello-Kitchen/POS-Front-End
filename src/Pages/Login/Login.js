5596330d0c1741204831d351224f0ccbe272ec71
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-06-27 15:46:33 +0200

    fix: Ajout redirection dans la page login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,41 @@
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import logo from "../Logo_Hello_Kitchen.png"
+import axios from "axios"
 
-
-function Login() {
-
+const Login = () => {
     const navigate = useNavigate();
-    const handleLogin = () => {
-        if (true) { // All info are correct
-            navigate("/dashboard")
-        }
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:3000/api/login/', {username, password})
+        .then((res) => {
+            navigate('/dashboard');
+        })
+        .catch((err) => {
+            console.log("ERROR", err)
+        })
     }
 
     return (
-        <div>
-            <text>Identifiant</text>
-            <text>Mot de passe</text>
-            <button onClick={() => handleLogin()}>Se Connecter</button>
+        <div className="login-container">
+            <img src={logo} alt="Logo" className="login-logo" />
+            <div className="login-box">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-field">
+                        <label htmlFor="username">Nom d'utilisateur</label>
+                        <input type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="password">Mot de passe</label>
+                        <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <button type="submit">Se connecter</button>
+                </form>
+            </div>
         </div>
     )
 }
