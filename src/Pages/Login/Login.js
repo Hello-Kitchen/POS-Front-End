b84aa720f5b0e1cecae473a8bb3ea0be3e585be8
Lucas_lgd02 <lucas.guiard@epitech.eu>
2024-06-27 17:08:43 +0200

    fix: Ajout tailwind css pour la page de login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,46 +1,59 @@
-import React, { useState } from "react"
-import "./Login.css"
-import logo from "./Logo_Hello_Kitchen.png"
-import axios from "axios"
-
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import logo from "./Logo_Hello_Kitchen.png";
+import axios from "axios";
 
 const Login = () => {
-
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await axios.post('http://localhost:4000/api/login/', {username, password})
+        await axios.post('http://localhost:3000/api/login/', {username, password})
         .then((res) => {
-            console.log("GOOD", res)
-            navigate("/dashboard")
+            navigate('/dashboard');
         })
         .catch((err) => {
-            console.log("ERROR", err)
-        })
-    }
+            console.log("ERROR", err);
+        });
+    };
 
     return (
-        <div className="login-container">
-            <img src={logo} alt="Logo" className="login-logo" />
-            <div className="login-box">
+        <div className="flex flex-col justify-center items-center h-screen bg-[#499CA6]">
+            <img src={logo} alt="Logo" className="w-52 h-52 mb-5" />
+            <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md w-80 text-center">
                 <form onSubmit={handleSubmit}>
-                    <div className="input-field">
-                        <label htmlFor="username">Nom d'utilisateur</label>
-                        <input type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)}/>
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block mb-2">Nom d'utilisateur</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="w-full p-2 rounded border border-gray-300"
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
                     </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Mot de passe</label>
-                        <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}/>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block mb-2">Mot de passe</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="w-full p-2 rounded border border-gray-300"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
-                    <button type="submit">Se connecter</button>
+                    <button
+                        type="submit"
+                        className="px-4 py-2 rounded bg-[#499CA6] text-white hover:bg-[#417f8c] transition duration-300"
+                    >
+                        Se connecter
+                    </button>
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Login;
