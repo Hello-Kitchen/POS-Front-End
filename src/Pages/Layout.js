d9bd94aacaab1aa037ca9970a07dda0069c32a88
Xantass <theo.fouque@epitech.eu>
2024-06-28 21:37:36 +0200

    feat: ajout du component d'affichage de la commande en cours. Celui comprend le plat, prix, detail, supplement et note avec un component Stop. Le compoenent est entierement modulable car il se base sur un tableau contenant le detail de chaque plat.

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -9,7 +9,7 @@ const Layout = () => {
         <div className="column w-full h-full">
             <LayoutHeader textLeft="05 - Francois Dupont" textCenter="Caisse 1" textRight="03/08/2024 - 12:35" />
             <div className="w-full h-4/5">
-                <Currentcommand />
+                <Currentcommand orders={[{nb: '42'}, [{plat: 'Hamburger', price: '15.60', details: ['Saignant', 'Frites', 'Salade'], sups: ['Supplement fromages', 'Allergie Oignons']}, {plat: 'Entrecote Classique', price: '22.30', details: ['Saignant', 'Frites', 'Salade'], note: 'Frites sans sel'}, {stop: true}, {plat: 'Brownie', price: '7.00'}, {plat: 'Hamburger', price: '15.60', details: ['Saignant', 'Frites', 'Salade'], sups: ['Supplement fromages', 'Allergie Oignons']}, {plat: 'Entrecote Classique', price: '22.30', details: ['Saignant', 'Frites', 'Salade'], note: 'Frites sans sel'}]]}/>
                 <Outlet />
             </div>
             <LayoutFooter buttons={["tables", "commandes", "transactions", "manager"]} price="44.90" />
