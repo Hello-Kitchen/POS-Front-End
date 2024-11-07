573c6e0e3a78f8f89247a8bcd3708162d5860255
Adrien le Corvec <adrien.le-corvec@epitech.eu>
2024-11-07 01:15:39 +0000

    add: jest plugin in eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,21 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
+import pluginJest from 'eslint-plugin-jest';
+
 export default [
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
@@ -13,6 +25,7 @@ export default [
         ecmaFeatures: { jsx: true },
       },
       globals: {
+        ...pluginJest.environments.globals.globals,
         ...globals.node,
         ...globals.browser,
       },
