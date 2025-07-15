7ce591e4d672779edb54b962a05dec6602dff625
Jules <jules.gresset@epitech.eu>
2025-07-14 21:55:50 -0400

    test: update API endpoint to use restaurant ID from localStorage

--- src/__tests__/Components/CurrentCommand.test.js
+++ src/__tests__/Components/CurrentCommand.test.js
@@ -159,7 +159,7 @@ describe('CurrentCommand', () => {
         await waitFor(() => {
             expect(global.fetch).toHaveBeenCalledTimes(1);
             expect(global.fetch).toHaveBeenCalledWith(
-                `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/1/orders${orders.tableId ? `?idTable=${orders.tableId}` : ''}`,
+                `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/${localStorage.getItem("restaurantID")}/orders${orders.tableId ? `?idTable=${orders.tableId}` : ''}`,
                 expect.objectContaining({
                     method: 'POST',
                     body: expect.any(String),
@@ -226,7 +226,7 @@ describe('CurrentCommand', () => {
         await waitFor(() => {
             expect(global.fetch).toHaveBeenCalledTimes(1);
             expect(global.fetch).toHaveBeenCalledWith(
-                `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/1/orders${orders.tableId ? `?idTable=${orders.tableId}` : ''}`,
+                `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}/api/${localStorage.getItem("restaurantID")}/orders${orders.tableId ? `?idTable=${orders.tableId}` : ''}`,
                 expect.objectContaining({
                     method: 'POST',
                     body: expect.any(String),
